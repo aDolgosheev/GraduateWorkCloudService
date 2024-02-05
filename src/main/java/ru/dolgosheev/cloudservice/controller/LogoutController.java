ac3630a8299c03ce003f19d4749173ee46a7717e
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-02-05 14:46:18 +0300

    update 2024.02.05

--- src/main/java/ru/dolgosheev/cloudservice/controller/LogoutController.java
+++ src/main/java/ru/dolgosheev/cloudservice/controller/LogoutController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.netology.cloudservice.service.AuthorizationService;
+import ru.dolgosheev.cloudservice.service.AuthorizationService;
 
 @RestController
 @RequestMapping("/logout")
